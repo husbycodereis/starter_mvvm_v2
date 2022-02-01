d87b0449ece45a4e842c99816a43e1202b01d1f9
Ali Rıza Reisoğlu <alirizareisoglu@gmail.com>
2022-02-01 18:52:55 +0300

    base fix

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      
       debugShowCheckedModeBanner: false,
       theme: context.watch<ThemeNotifier>().currentTheme,
       navigatorKey: NavigationService.instance.navigatorKey,
