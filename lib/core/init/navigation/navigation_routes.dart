8e1bfc4a44d8ba23e5f9e4e004c30495790c1cf3
Ali Rıza Reisoğlu <alirizareisoglu@gmail.com>
2022-02-24 12:45:22 +0300

    initial commit

--- lib/core/init/navigation/navigation_routes.dart
+++ lib/core/init/navigation/navigation_routes.dart
@@ -2,13 +2,10 @@ import 'package:flutter/material.dart';
 
 import 'package:movies_catalog/core/components/exceptions/navigate_model_exception.dart';
 import 'package:movies_catalog/view/authentication/splash/view/splash_view.dart';
-import 'package:movies_catalog/view/movie_details/view/movie_details_view.dart';
-import 'package:movies_catalog/view/search/model/movie_result.dart';
-import 'package:movies_catalog/view/search/view/search_view.dart';
+
 import 'package:movies_catalog/view/settings/model/settings_dynamic_model.dart';
 import 'package:movies_catalog/view/settings/view/subview/settings_dynamic_view.dart';
-import 'package:movies_catalog/view/watchlist/model/watchlist_model.dart';
-import 'package:movies_catalog/view/watchlist/view/subview/watchlist_movies_view.dart';
+
 
 import '../../../view/authentication/login/view/login_view.dart';
 import '../../../view/home/view/home_view.dart';
@@ -29,27 +26,12 @@ class NavigationRoute {
         return normalNavigate(const LoginView());
       case NavigationConstants.HOME:
         return normalNavigate(const HomeView());
-      case NavigationConstants.SEARCH:
-        if (args.arguments is bool) {
-          return normalNavigate(SearchView(isSubView: args.arguments! as bool));
-        }
-        throw NavigateException<SettingsDynamicModel>(args.arguments);
-      case NavigationConstants.MOVIE_DETAILS_VIEV:
-        if (args.arguments is MovieResultModel) {
-          return normalNavigate(MovieDetailsView(movie: args.arguments! as MovieResultModel));
-        }
-        throw NavigateException<SettingsDynamicModel>(args.arguments);
-      case NavigationConstants.WATCHLIST_MOVIES_VIEW:
-        if (args.arguments is WatchListModel) {
-          return normalNavigate(WatchListMoviesView(watchlist: args.arguments! as WatchListModel));
-        }
-        throw NavigateException<SettingsDynamicModel>(args.arguments);
-      case NavigationConstants.SETTINGS_WEB_VIEW:
+    case NavigationConstants.SETTINGS_WEB_VIEW:
         if (args.arguments is SettingsDynamicModel) {
           return normalNavigate(SettingsDynamicView(model: args.arguments! as SettingsDynamicModel));
         }
         throw NavigateException<SettingsDynamicModel>(args.arguments);
-
+   
       default:
         return normalNavigate(const NotFoundNavigation());
     }
--- lib/view/home/viewmodel/home_view_model.dart
+++ lib/view/home/viewmodel/home_view_model.dart
@@ -2,61 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
 import 'package:movies_catalog/core/base/model/base_view_model.dart';
 import 'package:movies_catalog/core/constants/navigation/navigation_constants.dart';
-import 'package:movies_catalog/view/home/service/IHomeService.dart';
-import 'package:movies_catalog/view/home/service/home_service.dart';
-import 'package:movies_catalog/view/search/model/movie_result.dart';
 part 'home_view_model.g.dart';
 
 class HomeViewModel = _HomeViewModelBase with _$HomeViewModel;
 
 abstract class _HomeViewModelBase with Store, BaseViewModel {
-  late IHomeService homeService;
-  late ScrollController movieScrollController;
   
   @override
   void setContext(BuildContext context) => this.context = context;
   @override
   void init() {
-    homeService = HomeService(vexanaManager.networkManager);
-    movieScrollController = ScrollController();
-    listenToMovieScrollController();
-    fetchTopMovies();
   }
 
-  void listenToMovieScrollController() {
-     movieScrollController.addListener(() {
-      if (movieScrollController.position.pixels == movieScrollController.position.maxScrollExtent) {
-        moviePageNumber++;
-        fetchTopMovies();
-      }
-    });
-  }
-
-  @observable
-  int moviePageNumber = 1;
-
-  @observable
-  bool loading = false;
-
-  @observable
-  List<MovieResultModel> topMoviesList = ObservableList.of([]);
-
-  @action
-  void setLoading() {
-    loading = !loading;
-  }
-
-  @action
-  Future fetchTopMovies() async {
-    final result = await homeService.fetchTopMovies(context!, moviePageNumber);
-    topMoviesList.addAll(result!.results ?? []);
-  }
-
-   void navigateToDetails(MovieResultModel movie) {
-    navigation.navigateToPage(path: NavigationConstants.MOVIE_DETAILS_VIEV, data: movie);
-  }
-
-  void dispose() {
-    movieScrollController.dispose();
-  }
 }
