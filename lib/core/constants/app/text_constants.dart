2220abc318576eb63c6e897988f155d7db469ac1
Ali Rıza Reisoğlu <alirizareisoglu@gmail.com>
2022-02-24 13:14:48 +0300

    text fix

--- lib/core/constants/app/text_constants.dart
+++ lib/core/constants/app/text_constants.dart
@@ -1,11 +1,11 @@
 class TextConstants {
-  static const movie_manager = 'The Movie Manager';
+  static const movie_manager = 'The Starter Project';
   static const login = 'Login';
   static const login_mail = 'Login with Email';
   static const login_mail_label = 'Email (eve.holt@reqres.in)';
   static const login_password = 'password (cityslicka)';
   static const login_forgot_password = 'Forgot Password?';
-  static const product_copyright = 'This product uses the TMDb API but is not endorsed or certified by TMDb.';
+  static const product_copyright = 'This product is a started project written in MVVM architecture pattern.';
   static const home_home = 'Home';
   static const home_search = 'Search';
   static const home_favorites = 'Favorites';
